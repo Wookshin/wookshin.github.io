e97b9e331b7502949af454e9bae859cd56c76134
wookshin <cotlsdnr1@gmail.com>
2021-07-06 08:20:48 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,25 @@
+function changeHandler(event) {
+    var display = document.querySelector('.display');
+    if ( this.value === 'list' ) {
+        if(display.classList.contains('box')){
+            display.classList.remove('box');
+        }
+        if(!display.classList.contains('list')){
+            display.classList.add('list');
+        }
+    } else if ( this.value === 'box' ) {
+        if(display.classList.contains('list')){
+        display.classList.remove('list');
+    }
+    if(!display.classList.contains('box')){
+        display.classList.add('box');
+    }
+    }  
+}
 
 window.addEventListener("load", () => {
     var radios = document.querySelectorAll('input[type=radio][name="display"]');
     
-    function changeHandler(event) {
-        var display = document.querySelector('.display');
-        if ( this.value === 'list' ) {
-            if(display.classList.contains('box')){
-                display.classList.remove('box');
-            }
-            if(!display.classList.contains('list')){
-                display.classList.add('list');
-            }
-        } else if ( this.value === 'box' ) {
-            if(display.classList.contains('list')){
-            display.classList.remove('list');
-        }
-        if(!display.classList.contains('box')){
-            display.classList.add('box');
-        }
-        }  
-    }
-    
-    if(radios[0].checked == true){
-        changeHandler.bind(radios[0]();
-    }
-    else if(radios[1].chekced){
-        changeHandler.bind(radios[1])();
-    }
-
     Array.prototype.forEach.call(radios, function(radio) {
         radio.addEventListener('change', changeHandler);
     });
@@ -36,4 +28,15 @@ window.addEventListener("load", () => {
     // $("div").click(function(){
     //     window.location=$(this).find("a").attr("href"); return false;
     //  });
+}, false);
+
+window.addEventListener("pageshow", () => {
+   var radios = document.querySelectorAll('input[type=radio][name="display"]');
+    
+   if(radios[0].checked == true){
+        changeHandler.bind(radios[0]();
+    }
+    else if(radios[1].chekced){
+        changeHandler.bind(radios[1])();
+    }
 }, false);
