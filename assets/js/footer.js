8d95fad148c60b07640bda079d666a3a528c4a93
wookshin <cotlsdnr1@gmail.com>
2021-12-10 11:11:25 +0900

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,9 +2,12 @@ const flicking = new Flicking("#flick", {
   renderOnlyVisible: true,
 });
 
+flicking.next();
+
 var posts = document.querySelectorAll(".flicking-panel");
 Array.prototype.forEach.call(posts, function (post) {
   post.addEventListener("click", function () {
     post.firstElementChild.click();
   });
 });
+
