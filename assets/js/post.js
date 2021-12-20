1c2841c28c2d522b182a4c76e31a39bdf66e1f9d
wookshin <cotlsdnr1@gmail.com>
2021-12-20 09:36:50 +0900

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,22 +1,39 @@
-window.addEventListener("load", () => {
-    document.getElementsByClassName('up')[0].addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        return false;
-    });
+window.addEventListener(
+  "load",
+  () => {
+    document.querySelector(".main-tags").classList.add("hidden");
 
-    document.getElementsByClassName('down')[0].addEventListener('click', function () {
-        window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth' });
+    document
+      .getElementsByClassName("up")[0]
+      .addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
         return false;
-    });
-}, false);
+      });
+
+    document
+      .getElementsByClassName("down")[0]
+      .addEventListener("click", function () {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth",
+        });
+        return false;
+      });
+  },
+  false
+);
 
 var isScrolling;
 
-window.addEventListener("scroll", () => {
-    window.clearTimeout( isScrolling );
-    var elem = document.querySelector('.arrow');
-    elem.style.display = 'block';
-    isScrolling = setTimeout(function() {
-        elem.style.display = 'none';
-	}, 1500);
-}, false);
+window.addEventListener(
+  "scroll",
+  () => {
+    window.clearTimeout(isScrolling);
+    var elem = document.querySelector(".arrow");
+    elem.style.display = "block";
+    isScrolling = setTimeout(function () {
+      elem.style.display = "none";
+    }, 1500);
+  },
+  false
+);
