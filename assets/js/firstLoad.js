ffce9623bc744b5666a33ad6512167316bea672f
wookshin <wookshins@gmail.com>
2022-02-19 12:12:13 +0900

    원복

--- assets/js/firstLoad.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.getItem("theme") === "light") {
-  document.documentElement.classList.remove("dark");
-  localStorage.setItem("theme", "light");
-} else {
-  document.documentElement.classList.add("dark");
-  localStorage.setItem("theme", "dark");
-}
