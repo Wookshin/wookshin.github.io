c74181031e8211dab0e8379a653175d1440e2b48
wookshin <wookshins@gmail.com>
2022-04-19 00:15:05 +0900

    TOC 구현

--- assets/js/post.js
+++ assets/js/post.js
@@ -10,3 +10,59 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+const tags = document.querySelectorAll('h1, h2')
+const tocItems = []
+const toc = document.querySelector('.document-toc')
+const tocHeader = document.querySelector('.document-toc-header')
+const tocList = document.querySelector('.document-toc-list')
+var focusedIdx = -1
+
+tags.forEach((tag, idx) => {
+	if (tag.tagName === 'H1') {
+		tocHeader.textContent = tags[0].textContent
+		tocHeader.dataset.idx = idx
+		tag.dataset.idx = idx
+		tocItems.push(tocHeader)
+	}
+	else {
+		let li = document.createElement('li')
+		li.classList.add('toc-list-item')
+		li.textContent = tag.textContent
+		li.dataset.idx = idx
+		tocList.appendChild(li)
+		tag.dataset.idx = idx
+		tocItems.push(li)
+	}
+}) 
+
+toc.addEventListener('click', e => {
+	let idx = e.target.dataset?.idx
+	if (idx !== -1) {
+		tags[idx].scrollIntoView({ behavior: "smooth", block: "center" })
+	}
+})
+
+
+const callback = (entries) => {
+    entries.forEach(entry => { 
+		if (entry.intersectionRatio <= 0) return
+		let idx = entry.target.dataset.idx
+		
+		if (!entry.isIntersecting && entry.boundingClientRect.y > 100) {
+			idx -= 1
+		}
+		
+		tocItems[focusedIdx]?.classList.remove('focus')
+		tocItems[idx].classList.add('focus')
+		focusedIdx = idx
+    })
+}
+
+const options = {
+  threshold: 1.0
+}
+
+const observer = new IntersectionObserver(callback, options);
+
+tags.forEach(tag => observer.observe(tag))
